cc9c2253e43bee8a4b794cd7cd337f15eb5efaba
paul <fca.21050@gmail.com>
2022-05-03 11:56:15 -0400

    'first'

---
+++ CBN.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+def news(ascending=False,counter=None,queryurl=None):
+
+    news={}
+    r=requests.get(f'https://www1.cbn.com/{queryurl}')
+
+    if r.status_code==200:
+        print(r.headers)
+
+        soup=BeautifulSoup(r.content,'html.parser')
+        news['results']=soup.find('div', class_='section-header-wrapper clearfix').find('span').text.strip()
+
+        for index,article in enumerate(soup.find_all('article')):
+            endpointurl=article['about']
+            if counter:
+                news['count'] = counter
+                if index<counter:
+                        try:
+                            news[index+1]={'title':article.find('span')['content'],'url':article['about'],'image':{'alt':article.find('img')['alt'],'src':article.find('img')['src'],'width':article.find('img')['width'],'height':article.find('img')['height'],'title':article.find('img')['title']}}
+                        except:
+                            news[index+1]={'title':article.find('span')['content'],'url':article['about'],'image':{'alt':article.find('img')['alt'],'src':article.find('img')['src'],'width':article.find('img')['width'],'height':article.find('img')['height']}}
+            else:
+
+                try:
+                    news[index+1] = {'title': article.find('span')['content'], 'url': article['about'],
+                                          'image': {'alt': article.find('img')['alt'],
+                                                    'src': article.find('img')['src'],
+                                                    'width': article.find('img')['width'],
+                                                    'height': article.find('img')['height'],
+                                                    'title': article.find('img')['title']}}
+                except:
+                    news[index+1] = {'title': article.find('span')['content'], 'url': article['about'],
+                                          'image': {'alt': article.find('img')['alt'],
+                                                    'src': article.find('img')['src'],
+                                                    'width': article.find('img')['width'],
+                                                    'height': article.find('img')['height']}}
+
+    return news
+
---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from typing import Optional,List
+import CBN
+app = FastAPI()
+
+
+
+@app.get("/api/v1/news/{types}")
+async def cbn_news(types:str,asc:Optional[bool]=False,count:Optional[int]=None):
+    if types=='us':
+
+        return CBN.news(counter=count,queryurl='cbnnews/us')
+    elif types=='world':
+
+        return CBN.news(counter=count, queryurl='cbnnews/cwn')
+    elif types=='top':
+
+        return CBN.news(counter=count, queryurl='cbnnews')
+    elif types == 'israel':
+        return CBN.news(counter=count, queryurl='cbnnews/israel')
+    elif types == 'national-security':
+        return CBN.news(counter=count, queryurl='cbnnews/national-security')
+    elif types == 'politics':
+        return CBN.news(counter=count, queryurl='cbnnews/faith-nation')
+    elif types == 'entertainment':
+        return CBN.news(counter=count, queryurl='cbnnews/entertainment')
+    elif types == 'health':
+        return CBN.news(counter=count, queryurl='cbnnews/health')
+    return {}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
